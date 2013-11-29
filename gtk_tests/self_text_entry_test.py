1963a755dbc3cba62dd1ada85e56de6149c8e7cb
James Wrigley <jwrigley7@gmail.com>
2013-11-29 21:36:22 +1300

    Finished self_text_entry_test.py

--- gtk_tests/self_text_entry_test.py
+++ gtk_tests/self_text_entry_test.py
@@ -7,24 +7,30 @@ class WindowMain(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Self Text Entry Test")
 
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        self.add(hbox)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.add(vbox)
+
+        hbox = Gtk.Box()
+        vbox.pack_start(hbox, True, True, 0)
 
         self.label = Gtk.Label(label="Enter text to be hashed: ")
         hbox.pack_start(self.label, True, True, 10)
 
         self.text_entry_widget = Gtk.Entry()
+        self.text_entry_widget.connect("activate", self.hash_text)
         hbox.pack_start(self.text_entry_widget, True, True, 0)
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-        hbox.pack_start(vbox, True, True, 0)
+        self.hash_button = Gtk.Button(label="Hash Text")
+        self.hash_button.props.halign = Gtk.Align.END
+        self.hash_button.set_size_request(150, 25)
+        self.hash_button.connect("clicked", self.hash_text)
+        vbox.pack_start(self.hash_button, False, False, 3)
 
-        self.get_text_button = Gtk.Button(label="Hash")
-        self.get_text_button.connect("clicked", self.hash_text(self.text_entry_widget.get_text))
-        vbox.pack_start(self.get_text_button, True, True, 0)
-
-    def hash_text(self, text):
-        print(hashlib.sha256(text.encode()).hexdigest())
+    def hash_text(self, unknown):
+        text_enc = self.text_entry_widget.get_text().encode()
+        m = hashlib.sha256()
+        m.update(text_enc)
+        print(m.hexdigest())
 
 main_window = WindowMain()
 main_window.connect("delete-event", Gtk.main_quit)
