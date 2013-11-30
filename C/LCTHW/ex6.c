991fac4fc5aed9bd52f8e618cd980a464ed8e061
James Wrigley <jwrigley7@gmail.com>
2013-12-01 09:00:36 +1300

    Added a new file for excersise 7 of LCTHW

--- C/LCTHW/ex6.c
+++ C/LCTHW/ex6.c
@@ -1,5 +1,7 @@
 // Code from exercise 6 of LCTHW
 
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
     int distance = 100;
@@ -10,4 +12,13 @@ int main(int argc, char *argv[])
     char last_name[] = "Wrigley";
 
     printf("You are %d miles away.\n", distance);
-    printf("You have %f levels of
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super powers.\n", super_power);
+    printf("I have an initial %c.\n", initial);
+    printf("I have a first name %s.\n", first_name);
+    printf("I have a last name %s.\n", last_name);
+    printf("My whole name is %s %c. %s.\n",
+	   first_name, initial, last_name);
+
+    return 0;
+}
