05f4c22137b3f7226e52242321a1a9e79f704fc9
James Wrigley <jwrigley7@gmail.com>
2013-12-06 20:02:20 +1300

    Added new program, one of my first that does something semi-useful

--- walker/walker.py
+++ walker/walker.py
@@ -3,10 +3,12 @@
 import sys
 import os
 
+if sys.platform == 'linux':
+    slash = "/"
+else:
+    slash = "\\"
+
 def delete_files(keyword):
-    '''
-    The find function
-    '''
 
     if len(keyword) == 1:
         print("You must enter a keyword!")
@@ -19,15 +21,17 @@ def delete_files(keyword):
     for root, dirs, files in os.walk(os.getcwd()):
         for file in files:
             if keyword in file:
-                print(root + "/" + file)
-                file_match_list.append(root + "/" + file)
+                print(root + slash + file)
+                file_match_list.append(root + slash + file)
 
-    if input("Are you sure you wish to delete these " + str(len(file_match_list)) + " files? [y/N]: ") in "ynYN":
-        print("Now to test!")
+    if file_match_list == []:
+        print("No matches found")
     else:
-        print
-#    for file in file_match_list:
-#        os.remove(file)
+        if input("Are you sure you wish to delete these " + str(len(file_match_list)) + " files? [y/N]: ") in "yY":
+            for file in file_match_list:
+                os.remove(file)
+        else:
+            print("Coward!" * len(file_match_list))
 
 if __name__ == '__main__':
-    delete_files(sys.argv)
+    delete_files(sys.argv) 
