9527632bddc9e6b800986331d104f9a2e3ca5912
James Wrigley <jwrigley7@gmail.com>
2013-12-11 13:20:20 +1300

    Getting there...

--- java/examples/Math.java
+++ java/examples/Math.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Math {
 
     public static void main(String[] args) {
-	Math MathClass = new Math();
+        Math MathClass = new Math();
+        BufferedReader read = new BufferedReader(new In 
 
-	Scanner kbdIn = new Scanner(System.in);
-	System.out.print("Press 1 to add, 2 to multiply, 3 to subtract, or 4 to divide: ");
+        Scanner kbdIn = new Scanner(System.in);
+        System.out.print("Press 1 to add, 2 to multiply, 3 to subtract, or 4 to divide: ");
+        int input = kbdIn.nextInt();
 
-    if (kbdIn.nextLine() == "1") {
-	MathClass.add(args);
-	    } else {
-	System.out.println(kbdIn.nextLine());
+        if (input == 1) {
+            MathClass.add();
+        } else {
+            System.out.println("NIH");
+        }
     }
-    }
 
-    public void add(String[] args) {
-	System.out.println("Foo bar");
+    public void add() {
+        String numbers = System.in.readline("Enter the numbers to be added: ");
+
+        System.out.println(numbers);
     }
 }
