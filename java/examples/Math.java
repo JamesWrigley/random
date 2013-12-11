33f7d86cdfb3a53e9ba44d12f4f3b8e4081ce02b
James Wrigley <jwrigley7@gmail.com>
2013-12-11 21:17:01 +1300

    Nearly got the add function working

--- java/examples/Math.java
+++ java/examples/Math.java
@@ -1,26 +1,44 @@
-import java.util.Scanner;
 import java.io.*;
+import java.util.Arrays;
 
 public class Math {
 
-    public static void main(String[] args) {
-        Math MathClass = new Math();
-        BufferedReader read = new BufferedReader(new In 
+    public static void main(String[] args)
+        throws Exception
+    {
+        Math mathClass = new Math();
 
-        Scanner kbdIn = new Scanner(System.in);
         System.out.print("Press 1 to add, 2 to multiply, 3 to subtract, or 4 to divide: ");
-        int input = kbdIn.nextInt();
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(read.readLine());
 
         if (input == 1) {
-            MathClass.add();
+            add();
         } else {
             System.out.println("NIH");
         }
     }
 
-    public void add() {
-        String numbers = System.in.readline("Enter the numbers to be added: ");
 
-        System.out.println(numbers);
+    public static void add() 
+        throws Exception
+    {
+        System.out.print("Enter the numbers to be added: ");
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        String[] values = read.readLine().split("\\s+");
+        int[] int_values = new int[values.length];
+
+        for (int i = 0; i < values.length; ++i) {
+            int_values[i] = Integer.parseInt(values[i]);
+        }
+
+        int sum = 0;
+        for (int i : int_values)
+            sum += i;
+
+        System.out.println(values);
+
     }
 }
