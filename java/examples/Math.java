a73d7eadc6668ab4ba0476f7e023d99bd95cf345
James Wrigley <jwrigley7@gmail.com>
2013-12-13 09:32:03 +1300

    Much cleaner version, thanks to JFo.nz

--- java/examples/Math.java
+++ java/examples/Math.java
@@ -1,113 +1,60 @@
+/* 
+ * 
+ * Issues: 
+ * Your code looked fairly tidy. 
+ * 
+ * 1) You have to watch out with integers and math operations. For example 1 / 2 = 0 with integers. I've changed the code to a float but you need to also beaware that floats are not suitable for operations that require accuracy. 
+ * 2) There is a lot of duplication of code. e.g. 4 * read integer code. You should put the duplicate / common code into a single method. 
+ * 3) You had a line of unused code ... Math mathClass = new Math();x 
+ * Here is a simplified version of the code 
+ */
+
 import java.io.*;
 import java.util.Arrays;
 
 public class Math {
 
-    public static void main(String[] args)
-        throws Exception
-    {
-        Math mathClass = new Math();
+    public static void main(String[] args) throws IOException {
+        run();
+    }
+ 
+    public static void run() throws IOException {
 
         System.out.print("Press 1 to add, 2 to multiply, 3 to subtract, or 4 to divide: ");
-
         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int input = Integer.parseInt(read.readLine());
+                
+        int input = Integer.parseInt(read.readLine()); //Ideally you should display an error rather than throw an exception
 
-        if (input == 1) {
-            add();
-        } else if (input == 2) {
-            multiply();
-        } else if (input == 3) {
-            subtract();
-        } else if (input == 4) {
-            divide();
-        } else {
-            System.out.println("NIH");
-        }
-    }
-
-
-    public static void add()
-        throws Exception
-    {
-        System.out.print("Enter the numbers to be added: ");
-
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        String[] values = read.readLine().split("\\s+");
-        int[] int_values = new int[values.length];
-
-        for (int i = 0; i < values.length; ++i) {
-            int_values[i] = Integer.parseInt(values[i]);
+        if(input != 1 && input != 2 && input != 3 && input != 4) { //Validate input 
+            System.out.println("Incorrect input"); 
+            return;
         }
 
-        int sum = 0;
-        for (int i : int_values) {
-            sum += i;
-        }
-        System.out.println(sum);
-    }
+        System.out.print("Enter numbers: ");
+        String[] values = read.readLine().trim().split("\\s+");
 
-    public static void multiply()
-        throws Exception
-    {
-        System.out.print("Enter the numbers to be multiplied: ");
+        float total = 0;
 
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        String[] values = read.readLine().split("\\s+");
-        int[] int_values = new int[values.length];
-        
-        for (int i = 0; i < values.length; ++i) {
-            int_values[i] = Integer.parseInt(values[i]);
-        }
+        for (int index = 0; index < values.length; index++) {
 
-        int sum = 1;
-        for (int i : int_values) {
-            sum *= i;
-        }
-        System.out.println(sum);
-    }
+            float floatValue = Float.parseFloat(values[index]); 
 
-    public static void subtract()
-        throws Exception
-    {
-        System.out.print("Enter the numbers to be subtracted (one after the other, in order of entry): ");
-        
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        String[] values = read.readLine().split("\\s+");
-        int[] int_values = new int[values.length];
-        
-        for (int i = 0; i < values.length; ++i) {
-            int_values[i] = Integer.parseInt(values[i]);
+            if(index == 0) { //Set value on first iteration 
+                total = floatValue;
+            }
+            else if (input == 1) {
+                total += floatValue;
+            } else if (input == 2) {
+                total *= floatValue;
+            } else if (input == 3) {
+                total -= floatValue;
+            }
+            else if (input == 4) {
+                total /= floatValue;
+            } else { //You should never hide a bug so throw exceptions for invalid states (Notice I throw an Error which doesn't have to be handled with a try catch statement)
+                throw new Error("Unknown input type: " + input);
+            }
         }
-        
-        int sum = int_values[0];
-        int i = 1;
-        while (i < int_values.length) {
-            sum -= int_values[i];
-            ++i;
-        }
-        System.out.println(sum);
-    }
-
-    public static void divide()
-        throws Exception
-    {
-        System.out.print("Enter the numbers to be divided (one after the other, in order of entry): ");
-        
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        String[] values = read.readLine().split("\\s+");
-        int[] int_values = new int[values.length];
-        
-        for (int i = 0; i < values.length; ++i) {
-            int_values[i] = Integer.parseInt(values[i]);
-        }
-        
-        int sum = int_values[0];
-        int i = 1;
-        while (i < int_values.length) {
-            sum /= int_values[i];
-            ++i;
-        }
-        System.out.println(sum);
+        System.out.println(total);
     }
 }
