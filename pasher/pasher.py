9929bc33b5888b329cf6cce8ce56a128830ecacf
James Wrigley <jwrigley7@gmail.com>
2013-12-14 13:41:24 +1300

    Didn't get very far, commiting as backup

--- pasher/pasher.py
+++ pasher/pasher.py
@@ -34,50 +34,43 @@ class MainWindow(Gtk.Window):
         select_object_menu.pack_start(object_menu_cell, False)
         select_object_menu.add_attribute(object_menu_cell, "text", 0)
         select_object_menu.set_active(0)
-        select_object_menu.connect("changed", methods.change_object, select_object_menu)
+        select_object_menu.connect("changed", change_object, select_object_menu)
         hbox.pack_start(select_object_menu, True, False, 0)
 
         string_entry_box = Gtk.Entry()
-        string_entry_box.connect("activate", methods.hash_string, string_entry_box)
+        string_entry_box.connect("activate", hash_string, string_entry_box)
         hbox.pack_start(string_entry_box, True, True, 10)
 
 
-class methods():
-    '''
-    Method definitions for pasher
-    '''
 
-    def __init__(self):
-        self.current_object_type = "String/Text"
-
-    def change_object(self, combo):
-        tree_iter = combo.get_active_iter()
-        model = combo.get_model()
-        self.current_object_type = model.get_value(tree_iter, 0)
-
-        if self.current_object_type == "File":
+def change_object(self, combo):
+    tree_iter = combo.get_active_iter()
+    model = combo.get_model()
+    current_object_type = model.get_value(tree_iter, 0)
+    
+    if current_object_type == "File":
+        window.string_entry_box.hide()
             
+def copy_to_clipboard(self, button, hashed_text):
+    clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD).set_text(hashed_text, -1)
 
-    def copy_to_clipboard(self, button, hashed_text):
-        clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD).set_text(hashed_text, -1)
-
-    def hash_string(self, entry_box):
-        text = entry_box.get_text()
-        hashed_text = hashlib.sha1(text.encode()).hexdigest()
- 
-        dialog = Gtk.Dialog()
-        dialog.set_size_request(400, 100)
-        dialog.set_title("Hash Result")
-        dialog.set_transient_for(MainWindow())
-        dialog.set_modal(True)
-        dialog.add_button("Copy to clipboard", Gtk.ResponseType.YES)
-        dialog.connect("response", methods.copy_to_clipboard, hashed_text)
-        dialog_content_area = dialog.get_content_area()
-        dialog_content_area_label = Gtk.Label(label="Hash of " + '"' + text + '"' + ":")
-        dialog_content_area.add(dialog_content_area_label)
-        hashed_text_label = Gtk.Label(label=hashed_text)
-        dialog_content_area.pack_start(hashed_text_label, True, True, 0)
-        dialog.show_all()
+def hash_string(self, entry_box):
+    text = entry_box.get_text()
+    hashed_text = hashlib.sha1(text.encode()).hexdigest()
+    
+    dialog = Gtk.Dialog()
+    dialog.set_size_request(400, 100)
+    dialog.set_title("Hash Result")
+    dialog.set_transient_for(MainWindow())
+    dialog.set_modal(True)
+    dialog.add_button("Copy to clipboard", Gtk.ResponseType.YES)
+    dialog.connect("response", copy_to_clipboard, hashed_text)
+    dialog_content_area = dialog.get_content_area()
+    dialog_content_area_label = Gtk.Label(label="Hash of " + '"' + text + '"' + ":")
+    dialog_content_area.add(dialog_content_area_label)
+    hashed_text_label = Gtk.Label(label=hashed_text)
+    dialog_content_area.pack_start(hashed_text_label, True, True, 0)
+    dialog.show_all()
 
 
 
