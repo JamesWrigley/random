d5712b795e44f4ea432517180ff4d4e0f71ec54e
James Wrigley <jwrigley7@gmail.com>
2014-01-08 16:26:39 +1300

    Made a few workflow changes to calculator.cpp and added support for floating point arithmetic

--- cpp/tests/calculator.cpp
+++ cpp/tests/calculator.cpp
@@ -7,55 +7,63 @@ using std::cin;
 
 int main()
 {
-  cout << "Press 1 to add, 2 to multiply, 3 to subtract, 4 to divide, or 5 to exit: ";
-
-  int input;
-  cin >> input;
-  std::cin.ignore();
-
-  if (input < 5)
+  while (true)
     {
-      // Some variable declarations
-      std::string numbers_str;
-      std::vector<int> numbers_int;
-      std::vector<std::string> numbers_vect;
-      std::string op_names[] = {"added", "multiplied", "subtracted", "divided"};
+      cout << "Press 1 to add, 2 to multiply, 3 to subtract, 4 to divide, or 5 to exit: ";
 
-      cout << "Enter 2 or more numbers to be " << op_names[input - 1] << ": ";
-      std::getline(cin, numbers_str);
+      int input;
+      cin >> input;
+      std::cin.ignore();
 
-      // Splits numbers_str by whitespace, then converts the string vector into an int vector
-      boost::split(numbers_vect, numbers_str, boost::is_any_of("\t "));
-      std::transform(begin(numbers_vect), end(numbers_vect), std::back_inserter(numbers_int), [](const std::string& val) { return std::stoi(val); });
+      if (input < 5)
+        {
+          // Some variable declarations
+          std::string numbers_str;
+          std::vector<float> numbers_float;
+          std::vector<std::string> numbers_vect;
+          std::string op_names[] = {"added", "multiplied", "subtracted", "divided"};
 
-      // The math part
-      if (1 == input)
-        {
-          int sum = std::accumulate(numbers_int.begin(), numbers_int.end(), 0);
-          cout << sum << std::endl;
+          cout << "Enter 2 or more numbers to be " << op_names[input - 1] << ": ";
+          std::getline(cin, numbers_str);
+
+          // Split numbers_str by whitespace, then converts the string vector into an int vector
+          boost::split(numbers_vect, numbers_str, boost::is_any_of("\t "));
+          std::transform(begin(numbers_vect), end(numbers_vect), std::back_inserter(numbers_float), [](const std::string& val) { return std::stof(val); });
+
+          // The math part
+          if (1 == input)
+            {
+              float sum = std::accumulate(numbers_float.begin(), numbers_float.end(), 0.0);
+              cout << sum << std::endl;
+              continue;
+            }
+          else if (2 == input)
+            {
+              float multiply_ans = std::accumulate(numbers_float.begin(), numbers_float.end(), 1.0, std::multiplies<float>());
+              cout << multiply_ans << std::endl;
+              continue;
+            }
+          else if (3 == input)
+            {
+              float subtract_ans = std::accumulate(numbers_float.begin() + 1, numbers_float.end(), numbers_float[0], std::minus<float>());
+              cout << subtract_ans << std::endl;
+              continue;
+            }
+          else if (4 == input)
+            {
+              float division_ans = std::accumulate(numbers_float.begin() + 1, numbers_float.end(), numbers_float[0], std::divides<float>());
+              cout << division_ans << std::endl;
+              continue;
+            }
         }
-      else if (2 == input)
+      if (5 == input)
         {
-          int multiply_ans = std::accumulate(numbers_int.begin(), numbers_int.end(), 1, std::multiplies<int>());
-          cout << multiply_ans << std::endl;
+          return 0;
         }
-      else if (3 == input)
+      else
         {
-          int subtract_ans = std::accumulate(numbers_int.begin() + 1, numbers_int.end(), numbers_int[0], std::minus<int>());
-          cout << subtract_ans << std::endl;
+          cout << "Invalid option \"" << input << "\"" << std::endl;
+          continue;
         }
-      else if (4 == input)
-        {
-          int division_ans = std::accumulate(numbers_int.begin() + 1, numbers_int.end(), numbers_int[0], std::divides<int>());
-          cout << division_ans << std::endl;
-        }
-    }
-  else if (5 == input)
-    {
-      return 0;
-    }
-  else
-    {
-      cout << "Invalid option \"" << input << "\"" << std::endl;
     }
 }
