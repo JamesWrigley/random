1779894db8a265bd7219bf3000851e052d7d6229
James Wrigley <jwrigley7@gmail.com>
2014-01-26 16:41:22 +1300

    Added soltution to problem 6 from Project Euler

---
+++ cpp/project_euler/6-sum_square_difference.cpp
@@ -0,0 +1,20 @@
+// Problem 6 from Project Euler
+// http://projecteuler.net/problem=6
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+  int max_number = 100;
+  int sum_of_squares;
+  int square_of_sum = pow(((max_number * (max_number + 1)) / 2), 2);
+
+  for (int i = 1; i <= max_number; i++)
+    {
+      sum_of_squares += pow(i, 2);
+      //      std::cout << sum_of_squares << std::endl;
+    }
+
+  std::cout << "The difference is " << square_of_sum - sum_of_squares << std::endl;
+}
