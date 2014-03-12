9eb3ade15aa20f8e71e16da38b203a63c2cbca67
James Wrigley <jwrigley7@gmail.com>
2014-03-12 22:12:34 +1300

    Learned a bit more

--- python/pyqt_tests/basic_window.py
+++ python/pyqt_tests/basic_window.py
@@ -1,19 +1,44 @@
 #! /usr/bin/python3
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
+
+class MainWindow(QtGui.QWidget):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
+
+        button = QtGui.QPushButton("Quit", self)
+        button.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        button.setToolTip("Quit Program")
+        button.resize(button.sizeHint())
+        button.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle("A Noob's Window")
+
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, "Message", "Are you sure you wish to quit?",
+                                           QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+
+        if reply == QtGui.QMessageBox.Yes:
+            print("Bye")
+            event.accept()
+        else:
+            event.ignore()
 
 def main():
-
     app = QtGui.QApplication(sys.argv)
-
-    window = QtGui.QWidget()
-    window.resize(250, 150)
-    window.move(300, 300)
-    window.setWindowTitle("A Noob's Window")
-    window.show()
-
+    window = MainWindow()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
