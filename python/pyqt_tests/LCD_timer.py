96daf0bc3ebe47562f824c1dc1fb3a5ef55fff9c
James Wrigley <jwrigley7@gmail.com>
2014-03-20 23:23:40 +1300

    Added WIP timer program and made some more improvements to the LCD_timer

--- python/pyqt_tests/LCD_timer.py
+++ python/pyqt_tests/LCD_timer.py
@@ -2,6 +2,7 @@
 
 
 import sys
+import time
 from PyQt4 import QtGui, QtCore
 
 class MainWindow(QtGui.QWidget):
@@ -16,16 +17,32 @@ class MainWindow(QtGui.QWidget):
         lcd_widget.display("00.00")
         numberEntry_widget = QtGui.QLineEdit(self)
         numberEntry_widget.setInputMask("00.00")
+        timer_start_button = QtGui.QPushButton("Start Timer")
 
         vbox = QtGui.QVBoxLayout()
         vbox.addWidget(lcd_widget)
-        vbox.addWidget(numberEntry_widget)
 
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(numberEntry_widget)
+        hbox.addWidget(timer_start_button)
+
+        vbox.addLayout(hbox)
         self.setLayout(vbox)
         
         self.resize(400, 250)
-        self.setWindowTitle("Timer")
+        self.setWindowTitle("YAT - Yet Another Timer")
         self.show()
+
+    def timer(self, value):
+        current_time = round(time.time(), 2)
+        end_time = current_time + value
+
+        while current_time <= end_time:
+            time.sleep(1)
+            self.lcd_widget
+            current_time += 1
+
+        return(0)
 
     def center(self):
         qr = self.frameGeometry()
