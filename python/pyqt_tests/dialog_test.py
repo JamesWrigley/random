1daaee45a44a9c56178223aca5a79e399bcdaba9
James Wrigley <jwrigley7@gmail.com>
2014-03-30 16:51:12 +1300

    Moar improvements...

--- python/pyqt_tests/dialog_test.py
+++ python/pyqt_tests/dialog_test.py
@@ -3,13 +3,42 @@
 import sys
 from PyQt4 import QtGui
 
-class MainWindow(QtGui.Qwidget):
+class MainWindow(QtGui.QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
 
 
     def initUI(self):
+        vbox = QtGui.QVBoxLayout()
+
+        self.button = QtGui.QPushButton("Dialog", self)
+        self.button.clicked.connect(self.showDialog)
+
+        self.text_edit = QtGui.QTextEdit(self)
+
+        vbox.addWidget(self.button)
+        vbox.addWidget(self.text_edit)
+
+        self.setLayout(vbox)
+        self.resize(400, 300)
+        self.setWindowTitle("Colors!")
+        self.center()
+        self.show()
+
+
+    def showDialog(self):
+        text, ok = QtGui.QInputDialog.getText(self, "Input Dialog", "Enter your name:")
+
+        if ok:
+            self.line_edit.setText(text)
+
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
 
 def main():
