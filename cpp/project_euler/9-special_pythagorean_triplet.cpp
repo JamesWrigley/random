dd53c12d4d0a537f17392f71fc9cafa8e038e5de
James Wrigley <jwrigley7@gmail.com>
2014-04-16 22:43:26 +1200

    Code cleanup.

--- cpp/project_euler/9-special_pythagorean_triplet.cpp
+++ cpp/project_euler/9-special_pythagorean_triplet.cpp
@@ -8,22 +8,17 @@
 
 int main()
 {
-  std::array<unsigned int, 1000> range;
-  std::iota(range.begin(), range.end(), 1); // Populate the range array
-
-  // We set 'a's upper bound to 997 so that the max value of 'int_a' will be 998,
-  // thus making sure that it will always be possible to get an 'int_b' and 'c' larger than 'a'
-  for (unsigned int a = 0; a <= 997; a++)
+  // We set 'a's upper bound to 998, thus making sure that it will always be 
+  // possible to get a 'b' and 'c' larger than 'a'
+  for (unsigned int a = 1; a <= 998; a++)
     {
-      unsigned int int_a = range[a];
-      for (unsigned int b = a + 1; b < 998; b++) // Note that 'b' will always be greater than 'a'
+      for (unsigned int b = a + 1; b <= 999; b++) // Note that 'b' will always be greater than 'a'
         {
-          unsigned int int_b = range[b];
-          unsigned int int_c = 1000 - (int_a + int_b);
+          unsigned int c = 1000 - (a + b);
 
-          if (pow(int_a, 2) + pow(int_b, 2) == pow(int_c, 2))
+          if (pow(a, 2) + pow(b, 2) == pow(c, 2))
             {
-              std::cout << int_a << " * " << int_b << " * " << int_c << " = " << int_a * int_b * int_c << std::endl;
+              std::cout << a << " * " << b << " * " << c << " = " << a * b * c << std::endl;
               return 0;
             }
         }
