499a858c8a7bc88c5d9d2946cb00fb8ff8791d1c
James Wrigley <jwrigley7@gmail.com>
2014-04-21 18:34:12 +1200

    The tenth solution now works, but is unscalable. Also switched back to GCC.

--- cpp/project_euler/10-summation_of_primes.cpp
+++ cpp/project_euler/10-summation_of_primes.cpp
@@ -1,20 +1,28 @@
 // Problem 10 from Project Euler
 // http://projecteuler.net/problem=10
 
-#include <cmath>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
-  std::vector<int> number_list(2000000);
+  std::vector<unsigned int> number_list(10000);
   std::iota(number_list.begin(), number_list.end(), 2);
 
-  for (unsigned int n = 0; n <= sqrt(10); n++)
+  for (unsigned int n = 0; n <= sqrt(number_list.size()); n++)
     {
-      for (unsigned int p = n + 1; p <= number_list.size(); p++)
+      unsigned int n_product = 0;
+
+      // While the product is less than the greatest element of number_list
+      while (n_product < number_list.back())
         {
-          if (fmod(number_list[p], number_list[n]) != 0
+          for (unsigned int multiplicand = 2; multiplicand < number_list.back(); ++multiplicand)
+            {
+              n_product = number_list[n] * multiplicand;
+              // Get rid of all occurences of n_product in number_list
+              number_list.erase(std::remove(number_list.begin(), number_list.end(), n_product), number_list.end());
+            }
         }
     }
 }
