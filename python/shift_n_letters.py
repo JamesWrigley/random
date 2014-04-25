469e5dd14a883a1c11649e3845264f3316f64297
James Wrigley <jwrigley7@gmail.com>
2014-04-25 18:04:27 +1200

    Finished shift_n_letters

--- python/shift_n_letters.py
+++ python/shift_n_letters.py
@@ -5,8 +5,14 @@
 import string
 
 def shift_n_letters(letter, steps):
-    letter_index = string.ascii_lowercase.index(letter) + 1 + steps
-    print(string.ascii_lowercase[letter_index])
+    letter_index = 0
+
+    if (string.ascii_lowercase.index(letter) + steps) <= 26:
+        letter_index = string.ascii_lowercase.index(letter) + steps
+        print(string.ascii_lowercase[letter_index])
+    else:
+        letter_index = (string.ascii_lowercase.index(letter) + steps) % 26
+        print(string.ascii_lowercase[letter_index])
 
 if __name__ == "__main__":
     user_input = list(input("Enter a letter and a number of steps: ").split())
