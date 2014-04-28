e73e635d0949ad5d2ef71e0dc7c9d16bd1c2d329
James Wrigley <jwrigley7@gmail.com>
2014-04-28 19:49:14 +1200

    Made some minor code improvements.

--- cpp/project_euler/10-summation_of_primes.cpp
+++ cpp/project_euler/10-summation_of_primes.cpp
@@ -1,15 +1,16 @@
 // Problem 10 from Project Euler
 // http://projecteuler.net/problem=10
 
+#include <cmath>
 #include <array>
 #include <iostream>
 
 int main()
 {
   const unsigned int size = 2000000;
+  long long primes_sum = 0;
   std::array<bool, size> bool_array;
   bool_array.fill(true);
-  // We set the number 1 to false here, it makes it a bit simpler
   bool_array[0] = false;
 
   // The Sieve of Eratosthenes
@@ -17,27 +18,18 @@ int main()
     {
       if (bool_array[number])
         {
+          primes_sum += number + 1;
           unsigned int multiplicand = 2;
-          // We increment 'number' by 1 to offset the zero indexing
+
+          // Increment 'number' by 1 to offset the zero numbering
           unsigned int n_product = multiplicand * (number + 1);
 
-          // While the product is less than the upper bound
           while (n_product <= size)
             {
               bool_array[n_product - 1] = false;
               ++multiplicand;
               n_product = multiplicand * (number + 1);
             }
-        }
-    }
-
-  // Add all the primes together
-  unsigned long long primes_sum = 0;
-  for (unsigned int i = 0; i < size; ++i)
-    {
-      if (bool_array[i])
-        {
-          primes_sum += i + 1;
         }
     }
 
