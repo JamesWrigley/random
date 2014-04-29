925a3a3d41a36fafbcb00924cfd371a77cb95bc3
James Wrigley <jwrigley7@gmail.com>
2014-04-29 19:23:54 +1200

    Added a couple of (nonfunctional) combo boxes and did some code cleanup.

--- python/pyqt_tests/QtHasher.py
+++ python/pyqt_tests/QtHasher.py
@@ -13,6 +13,9 @@ from PyQt4 import QtGui, QtCore
 
 class MainWindow(QtGui.QWidget):
 
+    algo_list = {"MD5":hashlib.md5, "SHA-1":hashlib.sha1, "SHA-224":hashlib.sha224,
+                 "SHA-256":hashlib.sha256, "SHA-384":hashlib.sha384, "SHA-512":hashlib.sha512}
+
     def __init__(self):
         super(MainWindow, self).__init__()
         self.initUI()
@@ -40,22 +43,38 @@ class MainWindow(QtGui.QWidget):
         # Widgets
         self.lineEdit_input = QtGui.QLineEdit(self)
         hash_button = QtGui.QPushButton("Hash")
+        object_type_comboBox = QtGui.QComboBox(self)
+        algos_comboBox = QtGui.QComboBox(self)
+
+        # Add items to algos_comboBox
+        #        for key in self.algo_list:
+        algos_comboBox.addItems(["MD5", "SHA-1", "SHA-224", "SHA-256", "SHA-384", "SHA-512"])
+
+        # Add items to object_type_comboBox
+        object_type_comboBox.addItem("String/Text")
+        object_type_comboBox.addItem("File")
+
 
         # Make connections
         hash_button.clicked.connect(lambda: hasher(self.lineEdit_input.text()))
         self.connect(self.lineEdit_input, QtCore.SIGNAL("enterPressed"), lambda: hasher(self.lineEdit_input.text()))
 
         # Make the layouts
-        vbox = QtGui.QVBoxLayout()
-        hbox = QtGui.QHBoxLayout()
+        main_vbox = QtGui.QVBoxLayout()  # Holds all the other layouts
+        upper_hbox = QtGui.QHBoxLayout() # Holds lineEdit_input and the hash object combo box
+        lower_hbox = QtGui.QHBoxLayout() # Holds the hash button and algos_comboBox
 
         # Packing
-        hbox.addStretch(1)
-        hbox.addWidget(hash_button)
-        vbox.addWidget(self.lineEdit_input)
-        vbox.addLayout(hbox)
+        upper_hbox.addWidget(object_type_comboBox)
+        upper_hbox.addWidget(self.lineEdit_input)
 
-        self.setLayout(vbox)
+        lower_hbox.addStretch()
+        lower_hbox.addWidget(algos_comboBox)
+        lower_hbox.addWidget(hash_button)
+        main_vbox.addLayout(upper_hbox)
+        main_vbox.addLayout(lower_hbox)
+
+        self.setLayout(main_vbox)
         self.setWindowTitle("QtHasher")
         self.center()
         self.show()
