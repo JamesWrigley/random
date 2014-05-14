e677a4191507eb6f230a826a404b4b03354527b6
James Wrigley <jwrigley7@gmail.com>
2014-05-14 21:27:23 +1200

    Added base code for the global mod frame.

--- python/moodloader/moodloader_ui.py
+++ python/moodloader/moodloader_ui.py
@@ -22,8 +22,10 @@ class MoodLoader(QtGui.QWidget):
         main_vbox = QtGui.QVBoxLayout()
         mods_hbox = QtGui.QHBoxLayout()
         mods_labels_hbox = QtGui.QHBoxLayout()
+
         map_mods_vbox = QtGui.QVBoxLayout()
         cam_mods_vbox = QtGui.QVBoxLayout()
+        global_mods_vbox = QtGui.QVBoxLayout()
 
 
         ### Make all the widgets ###
@@ -45,18 +47,29 @@ class MoodLoader(QtGui.QWidget):
         cam_mod_frame.setFrameShape(QtGui.QFrame.StyledPanel)
         cam_mod_frame.setLayout(cam_mods_vbox)
 
+        # Global mod widgets
+        global_mod_label = QtGui.QLabel("Global Mods")
+        install_global_mod_button = QtGui.QPushButton("Install Global Mod")
+        global_mod_frame = QtGui.QFrame()
+        global_mod_frame.setFrameShape(QtGui.QFrame.StyledPanel)
+        global_mod_frame.setLayout(global_mods_vbox)
+
 
         ### Pack everything ###
         mods_labels_hbox.addWidget(map_mod_label)
         mods_labels_hbox.addWidget(cam_mod_label)
+        mods_labels_hbox.addWidget(global_mod_label)
 
         map_mods_vbox.addWidget(install_map_mod_button)
         cam_mods_vbox.addWidget(install_cam_mod_button)
+        global_mods_vbox.addWidget(install_global_mod_button)
 
         mods_hbox.addWidget(map_mod_frame)
         mods_hbox.addWidget(cam_mod_frame)
+        mods_hbox.addWidget(global_mod_frame)
 
         main_vbox.addWidget(header_image_label)
+        main_vbox.addLayout(mods_labels_hbox)
         main_vbox.addLayout(mods_hbox)
         main_vbox.addStretch()
 
