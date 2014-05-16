22f6319293b221a7cbc03c0f1a5a2fb84cc65a29
James Wrigley <jwrigley7@gmail.com>
2014-05-16 19:53:51 +1200

    Removed debugging print() statement, and changed a variable name.

--- python/moodloader/moodloader.py
+++ python/moodloader/moodloader.py
@@ -16,21 +16,20 @@ class MainWindow(MoodLoader):
         Get the path of the config folder of the latest version of WZ on the
         users computer.
         """
-        config_dirs = []
-        config_dirs = [directory for directory in os.listdir(os.path.expanduser("~")) if ".warzone2100-" in directory]
+        matching_dirs = []
+        matching_dirs = [directory for directory in os.listdir(os.path.expanduser("~")) if ".warzone2100-" in directory]
 
-        if len(config_dirs) == 1:
-            return(os.path.expanduser("~") + "/" + config_dirs[0])
+        if len(matching_dirs) == 1:
+            return(os.path.expanduser("~") + "/" + matching_dirs[0])
         else:
             # Else we find the latest version number and append it to the base name
-            dir_versions = [float(re.findall(r'\d+\.\d+', foo)[0]) for directory in config_dirs]
+            dir_versions = [float(re.findall(r'\d+\.\d+', foo)[0]) for directory in matching_dirs]
             return(os.path.expanduser("~") + ".warzone2100-" + str(max(dir_versions)))
 
 
     def __init__(self):
         ### Create some system variables ###
         config_dir = self.get_config_path()
-        print(config_dir)
 
         super(MoodLoader, self).__init__()
         self.initUI()
