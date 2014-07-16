08a98b757d52111c5051c8285e49655493fb6439
James Wrigley <jwrigley7@gmail.com>
2014-07-17 09:25:47 +1200

    Moved a lot of code into separate methods.

--- cpp/qt-hasher/hasher.cpp
+++ cpp/qt-hasher/hasher.cpp
@@ -3,39 +3,15 @@
 
 Hasher::Hasher(QWidget *parent) : QWidget(parent)
 {
-  QStringList algos;
-  algos << "MD5" << "SHA-1" << "SHA-224" << "SHA-256" << "SHA-384" << "SHA-512";
-
-  QStringList object_types;
-  object_types << "String/Text" << "File";
-
-  // Create the layouts
-  QVBoxLayout *main_vbox = new QVBoxLayout(this);
-  QHBoxLayout *title_hbox = new QHBoxLayout();
-  QHBoxLayout *middle_hbox = new QHBoxLayout();
-  QHBoxLayout *bottom_hbox = new QHBoxLayout();
-
-  // Create widgets
-  QLabel *title = new QLabel("Qt Hasher", this);
-  title->setFont(QFont("Inconsolata", 40));
-
-  QLabel *object_type_label = new QLabel("Object type:", this);
-
-  QComboBox *object_types_comboBox = new QComboBox(this);
-  object_types_comboBox->addItems(object_types);
-
-  QComboBox *algos_comboBox = new QComboBox(this);
-  algos_comboBox->addItems(algos);
-  algos_comboBox->setCurrentIndex(1);
-
-  QLineEdit *lineEdit_input = new QLineEdit(this);
+  create_layouts();
+  create_widgets();
 
   // Packing
   title_hbox->addStretch();
-  title_hbox->addWidget(title);
+  title_hbox->addWidget(main_title);
   title_hbox->addStretch();
 
-  middle_hbox->addWidget(object_type_label);
+  middle_hbox->addWidget(object_types_label);
   middle_hbox->addWidget(algos_comboBox);
   middle_hbox->addWidget(lineEdit_input);
 
@@ -50,10 +26,41 @@ Hasher::Hasher(QWidget *parent) : QWidget(parent)
   this->setLayout(main_vbox);
   this->setWindowTitle("Qt Hasher");
   this->setFixedSize(375, 220);
-  this->center();
+  this->center_window();
 }
 
-void Hasher::center()
+
+void Hasher::create_layouts()
+{
+  main_vbox = new QVBoxLayout(this);
+  title_hbox = new QHBoxLayout();
+  middle_hbox = new QHBoxLayout();
+  bottom_hbox = new QHBoxLayout();
+}
+
+
+void Hasher::create_widgets()
+{
+  algos << "MD5" << "SHA-1" << "SHA-224" << "SHA-256" << "SHA-384" << "SHA-512";
+  object_types << "String/Text" << "File";
+
+  main_title = new QLabel("Qt Hasher", this);
+  main_title->setFont(QFont("Inconsolata", 40));
+
+  object_types_label = new QLabel("Object type:", this);
+
+  object_types_comboBox = new QComboBox(this);
+  object_types_comboBox->addItems(object_types);
+
+  algos_comboBox = new QComboBox(this);
+  algos_comboBox->addItems(algos);
+  algos_comboBox->setCurrentIndex(1);
+
+  lineEdit_input = new QLineEdit(this);
+}
+
+
+void Hasher::center_window()
 {
   QRect screen_geo = QApplication::desktop()->screenGeometry();
   QWidget *main_window = QWidget::window();
--- cpp/qt-hasher/hasher.h
+++ cpp/qt-hasher/hasher.h
@@ -1,7 +1,13 @@
 #ifndef HASHER_H
 #define HASHER_H
 
+#include <QLabel>
 #include <QWidget>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QStringList>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 class Hasher : public QWidget
 {
@@ -11,7 +17,23 @@ class Hasher : public QWidget
   Hasher(QWidget *parent = 0);
 
  private:
-  void center();
+  void center_window();
+  void create_layouts();
+  void create_widgets();
+
+  QVBoxLayout *main_vbox;
+  QHBoxLayout *title_hbox;
+  QHBoxLayout *middle_hbox;
+  QHBoxLayout *bottom_hbox;
+
+  QStringList algos;
+  QStringList object_types;
+  QLabel *main_title;
+  QLabel *object_types_label;
+  QComboBox *object_types_comboBox;
+  QComboBox *algos_comboBox;
+  QLineEdit *lineEdit_input;
+
 };
 
 #endif // HASHER_H
