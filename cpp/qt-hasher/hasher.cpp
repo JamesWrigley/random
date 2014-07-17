df702b3b0f33e41b8b908b684bcb64987d4857b8
James Wrigley <jwrigley7@gmail.com>
2014-07-17 12:34:53 +1200

    Moar improvements.

--- cpp/qt-hasher/hasher.cpp
+++ cpp/qt-hasher/hasher.cpp
@@ -1,4 +1,6 @@
-#include <QtGui>
+#include <QDir>
+#include <QApplication>
+#include <QDesktopWidget>
 #include "hasher.h"
 
 Hasher::Hasher(QWidget *parent) : QWidget(parent)
@@ -6,18 +8,27 @@ Hasher::Hasher(QWidget *parent) : QWidget(parent)
   create_layouts();
   create_widgets();
 
+  // Set up connections
+  QObject::connect(object_types_comboBox, SIGNAL(currentIndexChanged(int)),
+                   this, SLOT(change_object_type(int)));
+  QObject::connect(open_file_button, SIGNAL(clicked(bool)),
+                   this, SLOT(select_file()));
+
   // Packing
   title_hbox->addStretch();
   title_hbox->addWidget(main_title);
   title_hbox->addStretch();
 
   middle_hbox->addWidget(object_types_label);
-  middle_hbox->addWidget(algos_comboBox);
+  middle_hbox->addWidget(object_types_comboBox);
   middle_hbox->addWidget(lineEdit_input);
 
   bottom_hbox->addStretch();
-  bottom_hbox->addWidget(object_types_comboBox);
+  bottom_hbox->addWidget(algos_comboBox);
+  bottom_hbox->addWidget(open_file_button);
+  bottom_hbox->addWidget(hash_button);
 
+  main_vbox->addStretch();
   main_vbox->addLayout(title_hbox);
   main_vbox->addStretch();
   main_vbox->addLayout(middle_hbox);
@@ -46,17 +57,42 @@ void Hasher::create_widgets()
 
   main_title = new QLabel("Qt Hasher", this);
   main_title->setFont(QFont("Inconsolata", 40));
-
   object_types_label = new QLabel("Object type:", this);
+
+  open_file_button = new QPushButton("Open File", this);
+  open_file_button->setDisabled(true);
+  hash_button = new QPushButton("Hash", this);
 
   object_types_comboBox = new QComboBox(this);
   object_types_comboBox->addItems(object_types);
-
   algos_comboBox = new QComboBox(this);
   algos_comboBox->addItems(algos);
   algos_comboBox->setCurrentIndex(1);
 
   lineEdit_input = new QLineEdit(this);
+}
+
+
+void Hasher::select_file()
+{
+  lineEdit_input->setText(QFileDialog::getOpenFileName(this, "Select File", QDir::homePath()));
+}
+
+
+void Hasher::change_object_type(int index)
+{
+  if (index == 0)
+    {
+      open_file_button->setDisabled(true);
+      lineEdit_input->setReadOnly(false);
+      lineEdit_input->setText("");
+    }
+  else if (index == 1)
+    {
+      open_file_button->setDisabled(false);
+      lineEdit_input->setText("");
+      lineEdit_input->setReadOnly(true);
+    }
 }
 
 
--- cpp/qt-hasher/hasher.h
+++ cpp/qt-hasher/hasher.h
@@ -6,8 +6,10 @@
 #include <QComboBox>
 #include <QLineEdit>
 #include <QStringList>
+#include <QPushButton>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <QFileDialog>
 
 class Hasher : public QWidget
 {
@@ -16,24 +18,30 @@ class Hasher : public QWidget
  public:
   Hasher(QWidget *parent = 0);
 
+  private slots:
+    void change_object_type(int);
+    void select_file();
+
  private:
-  void center_window();
-  void create_layouts();
-  void create_widgets();
+    void center_window();
+    void create_layouts();
+    void create_widgets();
 
-  QVBoxLayout *main_vbox;
-  QHBoxLayout *title_hbox;
-  QHBoxLayout *middle_hbox;
-  QHBoxLayout *bottom_hbox;
+    QStringList algos;
+    QStringList object_types;
 
-  QStringList algos;
-  QStringList object_types;
-  QLabel *main_title;
-  QLabel *object_types_label;
-  QComboBox *object_types_comboBox;
-  QComboBox *algos_comboBox;
-  QLineEdit *lineEdit_input;
+    QVBoxLayout *main_vbox;
+    QHBoxLayout *title_hbox;
+    QHBoxLayout *middle_hbox;
+    QHBoxLayout *bottom_hbox;
 
+    QLabel *main_title;
+    QLabel *object_types_label;
+    QComboBox *object_types_comboBox;
+    QComboBox *algos_comboBox;
+    QLineEdit *lineEdit_input;
+    QPushButton *open_file_button;
+    QPushButton *hash_button;
 };
 
 #endif // HASHER_H
